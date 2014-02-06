13d0c7162c632c9e4e08da4f93ca925afedcb60f
Anisul <anisul.huq90@gmail.com>
2014-02-06 17:19:09 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spree2::Application.config.secret_key_base = '7e5a9805d439c8879d6e1635503de099b4b4fed656c44c9453574c6c33469d9803c1b3acbdbbcef5962747c4a493deef33bbff622291e51f727cbc02e5484b12'
---
+++ db/migrate/20140117211603_add_sale_price_to_spree_variants.spree_simple_sales.rb
@@ -0,0 +1,7 @@
+# This migration comes from spree_simple_sales (originally 20140117211251)
+class AddSalePriceToSpreeVariants < ActiveRecord::Migration
+  def change
+    add_column :spree_variants, :sale_price, :decimal, :precision => 8, :scale => 2
+  end
+end
+
