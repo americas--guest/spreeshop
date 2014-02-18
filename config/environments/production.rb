0c4aca1412484e370fbecf1d30da5275eb705040
Anisul <anisul.huq90@gmail.com>
2014-02-18 16:05:24 -0500

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Spree2::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
+  
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
