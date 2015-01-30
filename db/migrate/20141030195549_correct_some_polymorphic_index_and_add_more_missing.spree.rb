b2aaa1b65f64f06fb2e8b834d9394ae6164c2e7f
Anisul <anisul.huq90@gmail.com>
2015-01-30 11:44:46 -0600

    taxes db

--- db/migrate/20141030195549_correct_some_polymorphic_index_and_add_more_missing.spree.rb
+++ db/migrate/20141030195549_correct_some_polymorphic_index_and_add_more_missing.spree.rb
@@ -1,4 +1,3 @@
-# This migration comes from spree (originally 20140410150358)
 class CorrectSomePolymorphicIndexAndAddMoreMissing < ActiveRecord::Migration
   def change
     add_index :spree_addresses, :country_id
