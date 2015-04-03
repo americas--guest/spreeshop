abed1e9cf3f160640e5c3aca2a9455524d5cd9d9
Anisul <anisul.huq90@gmail.com>
2015-04-03 15:32:14 -0500

    spree

--- app/controllers/spree/products_controller.rb
+++ app/controllers/spree/products_controller.rb
@@ -1,7 +1,6 @@
 module Spree
   class ProductsController < Spree::StoreController
-    before_filter :authenticate_spree_user!
-    before_filter :load_product, :only => :show
+    before_filter :load_product, :only => :show, :authenticate_spree_user!
     rescue_from ActiveRecord::RecordNotFound, :with => :render_404
     helper 'spree/taxons'
 
