44597450434312649c0d4b54b4bc01d8fffe6604
Anisul <anisul.huq90@gmail.com>
2015-07-03 13:13:32 -0500

    sumo tryout

---
+++ app/overrides/add_smart_bar_to_head.rb
@@ -0,0 +1,7 @@
+Deface::Override.new(:virtual_path => "spree/shared/_head",
+
+:name => "add_smart_bar_to_header",
+
+:insert_after => "title",
+
+:text => '<script src="//load.sumome.com/" data-sumo-site-id="5e5dbde318cbb5a94330fd6e057238979578b4e50b0f21d90391580c3159f004" async="async"></script>')
